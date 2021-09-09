803a2a169528dd6a590965bf02d001fe6e4886f2
Maksym <maksimko312@gmail.com>
2021-09-09 23:15:02 +0300

    first commit
    Signed-off-by: Maksym <maksimko312@gmail.com>

---
+++ app/data/const.js
@@ -0,0 +1,11 @@
+const commands = `
+/start
+/help
+`
+
+const text = `
+Проверка ввода <a href="https://youtube.com/"> Youtube </a>
+`
+
+module.exports.commands = commands
+module.exports.text = text
